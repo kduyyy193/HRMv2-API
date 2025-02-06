8c45e04c136e178023e172f4d5c5f9b3760b6eae
K2 <122582194+kduyyyy1levinci@users.noreply.github.com>
2025-02-06 16:51:16 +0700

    Update user APIs

--- app/api/users.py
+++ app/api/users.py
@@ -12,6 +12,7 @@ from app.repositories.users import (
     create_user,
     get_user_by_email,
     get_user_by_username,
+    delete_user_by_id,
     get_users
 )
 
@@ -70,4 +71,9 @@ def delete_user(
     user_id: int, db: Session = Depends(get_db),
     admin: dict = Depends(get_admin_user)
 ):
-    return delete_user(user_id, db)
+    if not admin:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User not found"
+        )
+    return delete_user_by_id(db, user_id)
--- app/services/auth.py
+++ app/services/auth.py
@@ -2,6 +2,11 @@ from fastapi import Depends, HTTPException, Security
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from app.models.users import UserRole
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from app.repositories.users import get_user_by_username
+from sqlalchemy.orm import Session
+from typing import Optional
 from dotenv import load_dotenv
 import os
 
@@ -9,8 +14,11 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_current_user(token: str = Security(oauth2_scheme)):
@@ -36,3 +44,39 @@ def get_admin_user(current_user: dict = Depends(get_current_user)):
     if current_user["role"] != UserRole.admin:
         raise HTTPException(status_code=403, detail="Permission denied")
     return current_user
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    print(password)
+    return pwd_context.hash(password)
+
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = get_user_by_username(db, username)
+    if not user or not verify_password(password, user.password_hash):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    if (
+        SECRET_KEY is None
+        or ALGORITHM is None
+        or ACCESS_TOKEN_EXPIRE_MINUTES is None
+    ):
+        raise ValueError("Missing required environment variables.")
+
+    to_encode = data.copy()
+    if expires_delta is None:
+        expires_delta = timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))
+    expire = datetime.now(timezone.utc) + expires_delta
+    to_encode.update({"exp": expire})
+    try:
+        encoded_token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    except Exception as e:
+        raise ValueError(f"Error encoding the token: {str(e)}")
+    return encoded_token
